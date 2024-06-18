ff7e87f5a6ce78dbdadf8e3d8a60bab29ecdd2d7
santiocampo1 <sanntiocampo@gmail.com>
2024-06-18 07:07:39 -0300

    Client initial configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/tasks' element={<h1>Tasks Page</h1>} />
+        <Route path='/add-task' element={<h1>New Task</h1>} />
+        <Route path='/tasks/:id' element={<h1>Update Task</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
