753dcf58d93f36adb959abaf38bb3cd49b25304f
santiocampo1 <sanntiocampo@gmail.com>
2024-06-28 10:29:25 -0300

    CRUD tasks and controllers

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -13,6 +13,11 @@ const taskSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 
 }, { timestamps: true });
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -5,9 +5,9 @@ import { getAllTasks, getTask, createTask, deleteTask, updateTask } from "../con
 const router = new Router();
 
 router.get("/tasks", authRequired, getAllTasks);
-router.get("/tasks:id", authRequired, getTask);
+router.get("/tasks/:id", authRequired, getTask);
 router.post("/tasks", authRequired, createTask);
-router.delete("/tasks:id", authRequired, deleteTask);
-router.put("/tasks:id", authRequired, updateTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
+router.put("/tasks/:id", authRequired, updateTask);
 
 export default router;
