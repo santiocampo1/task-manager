8ddabd7dada2e311bf0f1ad65b4926eff1c560a0
santiocampo1 <sanntiocampo@gmail.com>
2024-06-28 09:30:54 -0300

    CRUD routes for tasks

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from'morgan';
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.routes.js';
 import cors from 'cors';
+
+// Routes
+import authRoutes from './routes/auth.routes.js';
+import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
 
@@ -12,6 +15,9 @@ app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
 }));
+
 app.use('/api', authRoutes);
+app.use('/api', tasksRoutes);
+
 
 export default app;
