91d03ed70c84d6baea40d7e440ae7e6e05d1a1a6
santiocampo1 <sanntiocampo@gmail.com>
2024-07-09 11:34:09 -0300

    Form styles

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,21 +1,24 @@
 import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-  const {register, handleSubmit, formState: {errors}} = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
   })
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="email"  {...register("email", { required: true })} placeholder="Email" />
-        {errors.email && (<p className="text-red-500 text-xs italic">Email is required</p>)}
-        <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="password"  {...register("password", { required: true })} placeholder="Password" />
-        {errors.password && (<p className="text-red-500 text-xs italic">Password is required</p>)}
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <h1 className="text-2xl font-bold">Login</h1>
+        <form onSubmit={onSubmit}>
+          <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="email"  {...register("email", { required: true })} placeholder="Email" />
+          {errors.email && (<p className="text-red-500 text-xs italic">Email is required</p>)}
+          <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="password"  {...register("password", { required: true })} placeholder="Password" />
+          {errors.password && (<p className="text-red-500 text-xs italic">Password is required</p>)}
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   )
 }
