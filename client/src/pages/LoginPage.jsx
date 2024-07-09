a14de850cc8fa879b7dbe873ee614809d41ca429
santiocampo1 <sanntiocampo@gmail.com>
2024-07-09 11:23:27 -0300

    Login Form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,23 @@
+import { useForm } from "react-hook-form";
+
 const LoginPage = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  })
+
   return (
-    <div>LoginPage</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="email"  {...register("email", { required: true })} placeholder="Email" />
+        {errors.email && (<p className="text-red-500 text-xs italic">Email is required</p>)}
+        <input className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" type="password"  {...register("password", { required: true })} placeholder="Password" />
+        {errors.password && (<p className="text-red-500 text-xs italic">Password is required</p>)}
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
-export default LoginPage
+export default LoginPage; 
