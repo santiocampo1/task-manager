ed2c49cf87065d1525c0517437ce3d3586c6d731
santiocampo1 <sanntiocampo@gmail.com>
2024-07-18 10:39:08 -0300

    Vercel config

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://tasksmanagercrud.netlify.app'  ,
     credentials: true
 }));
 
