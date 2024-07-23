ea2cbfac9746de26e06855c6469ed39eaaeb1061
santiocampo1 <sanntiocampo@gmail.com>
2024-07-23 09:18:32 -0300

    Vercel.json was added and modified

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://tasksmanager-gilt.vercel.app/api",
+    baseURL: "https://task-manager-mongodb.vercel.app/api",
     withCredentials: true,
 });
 
--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://tasksmanagercrud.netlify.app', 'https://tasksmanager-gilt.vercel.app'],
+    origin: ['http://localhost:5173', 'https://tasksmanagercrud.netlify.app/', 'https://task-manager-mongodb.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
