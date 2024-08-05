1bf10a748a82aa54b53fe55574f4a7d09c0aa56a
santiocampo1 <sanntiocampo@gmail.com>
2024-08-05 09:10:24 -0300

    Bugs with dates fixed

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,6 +1,10 @@
 import { useTasks } from "../context/TasksContext";
 import { Link } from "react-router-dom";
 
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+
 const TaskCard = ({ task }) => {
     const { deleteTask } = useTasks();
 
@@ -11,12 +15,14 @@ const TaskCard = ({ task }) => {
                 <div className="flex gap-x-2 items-center">
                     <button onClick={() => {
                         deleteTask(task._id);
-                    }}>Delete</button>
-                    <Link to={`/tasks/${task._id}`}>Edit</Link>
+                    }} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">Delete</button>
+                    <Link to={`/tasks/${task._id}`} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Edit</Link>
                 </div>
             </header>
             <p className="text-slate-300">{task.description}</p>
-            <p>{new Date(task.date).toLocaleDateString()}</p>
+            <p>
+                {dayjs(task.date).utc().format("DD/MM/YYYY")}
+            </p>
         </div>
     )
 }
