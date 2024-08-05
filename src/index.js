de33e81caf00ca0a7c00ca842918cd1a0ce9afc3
santiocampo1 <sanntiocampo@gmail.com>
2024-08-05 06:43:21 -0300

    gitignore and index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import app from './app.js';
 import { connectDB } from './db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 connectDB();
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 }); 
